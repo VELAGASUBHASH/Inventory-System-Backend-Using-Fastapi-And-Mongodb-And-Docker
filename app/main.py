39c4a3e8debb27658f54e78d3e467c71ecb35aae
VELAGA SUBHASH <130762017+VELAGASUBHASH@users.noreply.github.com>
2026-05-11 14:09:23 +0530

    Add new allowed origin for CORS

--- app/main.py
+++ app/main.py
@@ -11,7 +11,8 @@ app.add_middleware(
 
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://inventorysystem-ruddy.vercel.app"
     ],
 
     allow_credentials=True,
